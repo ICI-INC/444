125e418b766c3941d32fb0cf036e12617492d400
ICI-INC <166365607+ICI-INC@users.noreply.github.com>
2024-08-12 12:06:27 -0300

    Add files via upload

---
+++ dados/dados.js
@@ -0,0 +1,13 @@
+function logar(){
+
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if(login = "MarceloWick" && senha == "14298156"){ //marcelo
+        alert('Sucesso');
+        location.href = "https://ici-inc.github.io/entrar/Entrar.html";
+    }else{
+        
+    }
+            
+}
